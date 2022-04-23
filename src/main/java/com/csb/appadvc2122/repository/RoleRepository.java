3c1a4c6ebaa1e9faf89daf733e41944821543ec9
Jose Enrico Aquino <12020378@benilde.edu.ph>
2022-04-23 10:50:16 +0800

    updated RoleRepository

--- src/main/java/com/csb/appadvc2122/repository/RoleRepository.java
+++ src/main/java/com/csb/appadvc2122/repository/RoleRepository.java
@@ -5,14 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface RoleRepository extends CrudRepository<Role, Long>  {
 
-    /**
-     * We don't need to create an implementation of this method since Spring provides dynamic implementation for this
-     * interface.
-     * See {@link https://docs.spring.io/spring-data/jpa/docs/current/reference/html/}
-     *
-     * @param name target role name
-     * @return Role
-     */
+
     Role findByName(String name);
 
 }
